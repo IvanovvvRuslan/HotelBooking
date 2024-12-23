ff0cc655b2a9cbae08f5db8dd94fc01303033563
Ruslan <126184381+IvanovvvRuslan@users.noreply.github.com>
2024-12-23 18:11:06 +0200

    first commit

---
+++ FactoryApp/FactoryApp/FactoryService.cs
@@ -0,0 +1,46 @@
+﻿namespace FactoryApp;
+
+public class FactoryService
+{
+    private readonly FactoryModel _factory;
+    private readonly SemaphoreSlim _semaphore;
+    
+    public FactoryService()
+    {
+        _factory = new FactoryModel();
+        _semaphore = new SemaphoreSlim(1, 1);
+    }
+    
+    Task[] tasks = new Task[100];
+    
+    
+    public async Task AddParts(int amount)
+    {
+        await _semaphore.WaitAsync();
+        try
+        {
+            _factory.PartsInStock += amount;
+            Console.WriteLine($"{amount} detail added. Total amount: {_factory.PartsInStock}");
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+
+    public async Task TakeParts(int amount)
+    {
+        while (_factory.PartsInStock > 0)
+        {
+            try
+            {
+                _factory.PartsInStock -= amount;
+                Console.WriteLine($"{amount} detail taken. Total amount: {_factory.PartsInStock}");
+            }
+            finally
+            {
+                await _semaphore.WaitAsync();
+            }
+        }
+    }
+}
