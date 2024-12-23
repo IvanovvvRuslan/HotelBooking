b131b6bce12826d28fc61929ac4db2802582020a
Ruslan <126184381+IvanovvvRuslan@users.noreply.github.com>
2024-12-23 18:12:38 +0200

    another first commit

---
+++ ConsoleApp1/ConsoleApp1/Counter.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleApp1;
+
+public class Counter
+{
+    public int Value { get; set; }
+}
---
+++ FactoryApp/FactoryApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using FactoryApp;
+
+FactoryService factoryService = new FactoryService();
+Task[] tasks = new Task[100];
+
+for (int i = 0; i < 100; i++)
+{
+    tasks[i] = Task.Run(() => factoryService.AddParts(1));
+    
+    tasks[i] = Task.Run(() => factoryService.TakeParts(1));
+    
+}
+
+await Task.WhenAll(tasks);
+Console.WriteLine("Done!");
