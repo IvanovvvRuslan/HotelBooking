642215d53f7c11258bd2e1076b4426d8ff12bcf4
Ruslan <126184381+IvanovvvRuslan@users.noreply.github.com>
2025-02-28 16:18:07 +0200

    Generic repository edited

--- HotelBooking/HotelBooking/Repositories/IGenericRepository.cs
+++ HotelBooking/HotelBooking/Repositories/IGenericRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace HotelBooking.Repositories;
+﻿using HotelBooking.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Repositories;
 
 public interface IGenericRepository<T> where T : class
 {
@@ -8,4 +11,50 @@ public interface IGenericRepository<T> where T : class
     Task CreateAsync(T entity);
     Task Delete(T entity);
     Task SaveChangesAsync();
+}
+
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    private readonly ApplicationDbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+    
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<T?> GetByIdTrackedAsync(int id)
+    {
+        return await _dbSet
+            .FirstOrDefaultAsync(e => EF.Property<int>(e, "Id") == id);
+    }
+
+    public async Task CreateAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public Task Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
 }
